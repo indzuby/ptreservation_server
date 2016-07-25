4f66041db0a563100c8beafa5dc256f58e7ad009
zuby <user1@user1ui-MacBook-Pro.local>
2016-07-25 22:26:16 +0900

    no message

--- src/db/migrate/20160419052526_add_column_customers.rb
+++ src/db/migrate/20160419052526_add_column_customers.rb
@@ -1,5 +1,7 @@
 class AddColumnCustomers < ActiveRecord::Migration
   def change
+    add_column :customers, :pt_count,:integer
+
     change_column_null :customers, :pt_count,false
     change_column_default :customers, :pt_count,0
   end
